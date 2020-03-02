9b50d83333e6ed4ed216749f08f2ccbb159a27cd
Ali Alnoaimi <3664619-ali_bh@users.noreply.gitlab.com>
2020-03-02 23:35:40 +0300

    Fixing the title

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: '%s',
     title: 'Basma Video',
     meta: [
       { charset: 'utf-8' },
