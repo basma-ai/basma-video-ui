3bc082219b90834499d21c7ac2f9b1fc47b47581
Munther <munther96@gmail.com>
2020-04-19 01:10:51 +0300

    Set default localization to english.

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -12,7 +12,7 @@ import VueI18n from "vue-i18n";
 
 const i18n = new VueI18n({
   locale: "en", // set locale
-  fallbackLocale: "ar" // set fallback locale
+  fallbackLocale: "en" // set fallback locale
   //   messages // set locale messages
 });
 
